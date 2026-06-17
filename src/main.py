b66276b42b0886c96674dca626f2fe02352b6c37
frankiejgit <frankiejgram@gmail.com>
2026-06-16 21:01:40 -0700

    Demo run: testing CI/CD trigger by changing demo version to 1.0

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Enterprise CI/CD Demo: Version 0.1"
+    return "Enterprise CI/CD Demo: Version 1.0"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
